cffcff924605cb18755e898f2e26ba8eb468292c
mubassam ali <mubassamali128@gmail.com>
2024-12-18 17:41:04 +0500

    E2

---
+++ src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome(props){
+    return(
+        <div>
+            <h1>Welcome Back {props.username}</h1>
+        </div>
+    );
+}
+
+export default Welcome
---
+++ src/styles/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from '../pages/Welcome/Welcome';
+
+
+function App(){
+  return(
+    <div>
+      <Welcome username="Mubassam Ali" />
+    </div>
+  );
+}
+export default App;
