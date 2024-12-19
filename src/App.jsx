591db5bbee416bbf8f61a3f4f4946a6775508d5a
mubassam ali <mubassamali128@gmail.com>
2024-12-19 11:52:01 +0500

    A1 10% Done!

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Profile from "./pages/Profile/Profile";
 
 const App = () => {
   const showPage = (page) => {
-    document.querySelectorAll(".page").forEach((el) => (el.style.display = "none"));
+    document.querySelectorAll(".page").forEach((pag) => (pag.style.display = "none"));
     document.getElementById(page).style.display = "block";
   };
 
   return (
     <div>
-      <div id="LandingPage" className="page" style={{ display: "block" }}>
+      <div id="LandingPage" className="page block">
         <LandingPage goToNextPage={() => showPage("Home")} />
       </div>
-      <div id="Home" className="page" style={{ display: "none" }}>
+      <div id="Home" className="page hidden">
         <Home goToNextPage={() => showPage("Profile")} />
       </div>
-      <div id="Profile" className="page" style={{ display: "none" }}>
+      <div id="Profile" className="page hidden">
         <Profile goToNextPage={() => showPage("LandingPage")} />
       </div>
     </div>
